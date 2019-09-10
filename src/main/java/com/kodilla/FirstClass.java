bd02949996f88eedf54d7b7a0d0ce3643e4aac17
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-10 17:57:14 +0200

    FirstClass cleared

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -3,26 +3,4 @@ package com.kodilla;
 public class FirstClass {
 
 
-    public int GetSum(int a, int b) {
-        int sum = 0;
-
-        if (a == b) {
-            return a;
-        }
-        if (a < b) {
-            for (int i = a; i < b + 1; i++) {
-                sum += a;
-                return sum;
-            }
-            if (a > b) {
-                for (int i = b; i < a + 1; i++) {
-                    sum += b;
-                    return sum;
-                }
-            }
-
-        }
-
-
-    }
 }
