52fa3de4caa4e8f9d7e8e9e2b7735add182914c7
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-10 17:48:01 +0200

    KodillaCourseApplication cleared

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -2,5 +2,27 @@ package com.kodilla;
 
 public class FirstClass {
 
+
+    public int GetSum(int a, int b) {
+        int sum = 0;
+
+        if (a == b) {
+            return a;
+        }
+        if (a < b) {
+            for (int i = a; i < b + 1; i++) {
+                sum += a;
+                return sum;
+            }
+            if (a > b) {
+                for (int i = b; i < a + 1; i++) {
+                    sum += b;
+                    return sum;
+                }
+            }
+
+        }
+
+
     }
 }
