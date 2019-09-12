0462e7e300b228da708fe24ccda342b1aed8f8a6
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-12 20:14:58 +0200

    Task 6.2 finished.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        if(Calculator.addAToB(6,4)==10) {
+            System.out.println("Add test Ok!");
+        } else {
+            System.out.println("Add test Nok!");
+        }
+
+        if(Calculator.subtractBFromA(6,4)==2) {
+            System.out.println("Subtract test Ok!");
+        } else {
+            System.out.println("Subtract test Nok!");
+        }
+
+
+
     }
+
+
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static int addAToB(int a, int b){
+        return a+b;
+    }
+    public static int subtractBFromA(int a, int b){
+        return a-b;
+    }
+
+
+}
