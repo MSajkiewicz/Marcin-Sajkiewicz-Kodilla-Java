efac99bbc91a26c0a83fe22b5db553c32cb405ac
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-15 16:46:32 +0200

    Task 6.3 finished:.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,20 +6,6 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
 
-        if(Calculator.addAToB(6,4)==10) {
-            System.out.println("Add test Ok!");
-        } else {
-            System.out.println("Add test Nok!");
-        }
-
-        if(Calculator.subtractBFromA(6,4)==2) {
-            System.out.println("Subtract test Ok!");
-        } else {
-            System.out.println("Subtract test Nok!");
-        }
-
-
-
     }
 
 
