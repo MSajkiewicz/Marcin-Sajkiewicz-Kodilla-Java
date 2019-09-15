6ace4a847feef0f66207eb4a8f28bba0acd5db12
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-15 21:51:53 +0200

    Task 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,18 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+    private ArrayList<Integer> evenList = new ArrayList<Integer>();
+
+    public void exterminate(ArrayList<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number % 2 == 0) evenList.add(number);
+        }
+    }
+
+    public ArrayList<Integer> getEvenList(){
+        return evenList;
+    }
+
 }
