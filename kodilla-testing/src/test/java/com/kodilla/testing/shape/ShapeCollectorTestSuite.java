d308888cf5176e071fc1b9aebb9faa563410ee98
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-17 17:11:19 +0200

    ShapeCollectorTestSuite added.

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,59 @@
+package com.kodilla.testing.shape;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShapeCollectorTestSuite {
+
+    @Test
+    public void testAddFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle("circle", 5);
+        //When
+        shapeCollector.addFigure(circle);
+        //Then
+        Assert.assertEquals(1, shapeCollector.getShapeListQuantity());
+
+    }
+
+    @Test
+    public void testRemoveFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle("circle", 5);
+        shapeCollector.addFigure(circle);
+        //When
+        shapeCollector.removeFigure(circle);
+        //Then
+        Assert.assertEquals(0, shapeCollector.getShapeListQuantity());
+
+    }
+
+    @Test
+    public void testGetFigure() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle("circle", 5);
+        shapeCollector.addFigure(circle);
+        //When & Then
+        Assert.assertEquals(circle, shapeCollector.getFigure(0));
+
+    }
+    @Test
+    public void getShapeListQuantity() {
+        //Given
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Circle circle = new Circle("circle", 5);
+        Triangle triangle = new Triangle ("triangle", 10);
+        Square square = new Square("square", 23);
+        //When
+        shapeCollector.addFigure(circle);
+        shapeCollector.addFigure(triangle);
+        shapeCollector.addFigure(square);
+        //Then
+        Assert.assertEquals(3, shapeCollector.getShapeListQuantity());
+
+    }
+
+}
