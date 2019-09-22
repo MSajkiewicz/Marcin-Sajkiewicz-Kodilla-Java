7d4a290ee53bb516fa159b023daf995b9ca93668
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-09-22 08:58:19 +0200

    Task 7.3 - date condition changed.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.util.Calendar;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,7 +13,7 @@ public class StreamMain {
 
         Map<Integer, ForumUser> theMapOfUsers = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getDateOfBirth().getYear() < 1999)
+                .filter(forumUser -> forumUser.getDateOfBirth().getYear() < Calendar.getInstance().get(Calendar.YEAR)-20)
                 .filter(forumUser -> forumUser.getCreatedPostsAmount() > 0)
                 .collect(Collectors.toMap(ForumUser::getUniqueID, forumUser -> forumUser));
 
