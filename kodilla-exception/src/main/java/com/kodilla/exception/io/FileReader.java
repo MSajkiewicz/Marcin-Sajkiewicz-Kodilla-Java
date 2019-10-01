a6b3674763d6f88ae04e929647160dd0aa2fad56
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-10-01 19:53:12 +0200

    Practice 8.1 update

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -5,23 +5,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileReader {
 
     public void readFile() throws FileReaderException{
         ClassLoader classLoader = getClass().getClassLoader();
+
+
+        //Files.lines(Paths.get(new File(getClass().getClassLoader().getResource("file/names.txt").getFile()).getPath())).collect(Collectors.joining(" "));
+        try {
+            String value = Files.lines(Paths.get("C:/Users/marci/IdeaProjects/Marcin-Sajkiewicz-Kodilla-Java/kodilla-exception/src/main/resources/file/names.txt")).collect(Collectors.joining(""));
+            System.out.println(value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
         try(Stream<String> fileLines = Files.lines(Paths.get("test.txt"))){
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            throw new FileReaderException();
-        } finally {
-            System.out.println("I am gonna be here... always!");
-        }
+                throw new FileReaderException();
+                } finally {
+                System.out.println("I am gonna be here... always!");
+                }
 
-    }
+                }
 
 
-}
+                }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,8 @@
+package com.kodilla.exception.test;
+
+public class FlightFinder {
+    public void findFlight(Flight flight) {
+
+    }
+
+}
