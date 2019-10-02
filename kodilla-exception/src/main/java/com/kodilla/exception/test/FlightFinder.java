d40ae41799633fb817d60df32d59f9640c6aef3c
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2019-10-02 18:08:53 +0200

    Task 8.4.

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -1,7 +1,21 @@
 package com.kodilla.exception.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlightFinder {
-    public void findFlight(Flight flight) {
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> mapOfPossibleFlights = new HashMap<>();
+        mapOfPossibleFlights.put("Krakow", true);
+        mapOfPossibleFlights.put("Katowice", true);
+        mapOfPossibleFlights.put("Gdansk", false);
+
+        Boolean result = mapOfPossibleFlights.get(flight.getArrivalAirport());
+            if(result != null) {
+                System.out.println(result);
+            } else {
+                throw new RouteNotFoundException();
+            }
 
     }
 
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,4 +1,6 @@
 package com.kodilla.exception.test;
 
-public class RouteNotFoundException {
+public class RouteNotFoundException extends Exception {
+
 }
+
