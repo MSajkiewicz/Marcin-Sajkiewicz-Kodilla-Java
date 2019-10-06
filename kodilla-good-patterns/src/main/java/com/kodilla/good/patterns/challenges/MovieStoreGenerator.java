e4b7b95e875070247c9834764a54e4bd6f021113
MSajkiewicz <54741962+MSajkiewicz@users.noreply.github.com>
2019-10-06 09:48:50 +0200

    Task 9.1. finished. (#16)

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreGenerator.java
@@ -0,0 +1,21 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.*;
+
+public final class MovieStoreGenerator {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String moviesString = movieStore.getMovies().entrySet().stream()
+                .flatMap(movie -> movie.getValue().stream())
+                .collect(Collectors.joining("!", "***", "***"));
+
+        System.out.println(moviesString);
+
+
+
+
+
+    }
+}
